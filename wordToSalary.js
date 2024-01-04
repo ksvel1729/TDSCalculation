4e6c68773c1b6535d9cb1a0af954fa9d80e803c0
ksvel1729 <82051348+ksvel1729@users.noreply.github.com>
2024-01-04 16:18:37 +0530

    adding excel version

--- wordToSalary.js
+++ wordToSalary.js
@@ -15,3 +15,25 @@ b.forEach((a)=>{
 })
 c+="109~"+sourceAccountNo+"~2~"+z.split('\t')[3]+"~WAGES"
 console.assert(total==z.split('\t')[3])
+
+// excel to salary
+let b=a.split('\n')
+let c=""
+let z
+let total=0
+let flag=true
+let sourceAccountNo="enterAccountnumber"
+b.forEach((a)=>{
+    if (!flag)return
+    if (a.indexOf('ACCOUNT')>-1&&a.indexOf('AMOUNT')>-1) return
+    if (a.indexOf('TOTAL')>-1) {z=a; flag=false; return}
+    sal=a.split('\t')[3].replace(',','')
+    if (sal.trim()=='-') sal=0 
+    total+=Number(sal)
+    c+="1~"+a.split('\t')[2]+"~1~"+sal+"~WAGES\n"
+    
+})
+c+="109~"+sourceAccountNo+"~2~"+z.split('\t')[3]+"~WAGES"
+console.log(total)
+console.log(Number(z.split('\t')[3].replaceAll(',','')))
+console.assert(total==Number(z.split('\t')[3].replaceAll(',','')))
